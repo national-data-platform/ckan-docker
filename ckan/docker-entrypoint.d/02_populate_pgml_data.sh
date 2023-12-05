8f2a27d2b42a1b91a48af937a319ef44c02d8abb
sergeygurvich <segurvich@ucsd.edu>
2023-12-04 16:47:16 -0800

    Commented out switching to branch

--- ckan/docker-entrypoint.d/02_populate_pgml_data.sh
+++ ckan/docker-entrypoint.d/02_populate_pgml_data.sh
@@ -3,7 +3,7 @@
 echo "Cloning dataloader repo and install requirements"
 git clone https://github.com/national-data-platform/ckan-data-loader;
 cd ckan-data-loader;
-git checkout pgml_data_script;
+#git checkout pgml_data_script;
 pip install -r requirements.txt
 echo "Run data population script in the background"
 source ./pgml_data_script.sh &
