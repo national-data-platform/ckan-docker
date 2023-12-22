7abb27c9c2e1711e042126dd12875bde5cc52901
sergeygurvich <segurvich@ucsd.edu>
2023-12-21 18:33:58 -0800

    Ckan_admin group in example.env Removed checkout branch in data population scripts

--- ckan/docker-entrypoint.d/02_prepopulate_data.sh
+++ ckan/docker-entrypoint.d/02_prepopulate_data.sh
@@ -3,7 +3,6 @@
 echo "Cloning dataloader repo and install requirements"
 git clone https://github.com/national-data-platform/ckan-data-loader;
 cd ckan-data-loader;
-#git checkout pgml_data_script;
 pip install -r requirements.txt
 echo "Run data population script in the background"
 source ./prepopulate_data_script.sh &
