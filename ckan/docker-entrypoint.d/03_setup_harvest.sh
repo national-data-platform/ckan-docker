03ac40e4040d71157dd9d923306f869575e60fd4
sergeygurvich <segurvich@ucsd.edu>
2024-02-21 18:44:59 -0800

    Changes for geoview ckan extension

--- ckan/docker-entrypoint.d/03_setup_harvest.sh
+++ ckan/docker-entrypoint.d/03_setup_harvest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Cloning dataloader repo and install requirements"
-git clone https://github.com/national-data-platform/ckan-data-loader;
+git clone https://github.com/national-data-platform/ckan-data-loader -b feature-copy-harvested-org-logos;
 cd ckan-data-loader;
 pip install -r requirements.txt
 echo "Run harvest setup in the background"
