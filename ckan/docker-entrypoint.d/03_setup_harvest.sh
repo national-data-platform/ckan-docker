4144f8af77ad76cb7baa1fe8f0e48d4e19e060f8
sergeygurvich <segurvich@ucsd.edu>
2024-02-21 20:05:17 -0800

    Removed using branch of data-loader

--- ckan/docker-entrypoint.d/03_setup_harvest.sh
+++ ckan/docker-entrypoint.d/03_setup_harvest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Cloning dataloader repo and install requirements"
-git clone https://github.com/national-data-platform/ckan-data-loader -b feature-copy-harvested-org-logos;
+git clone https://github.com/national-data-platform/ckan-data-loader
 cd ckan-data-loader;
 pip install -r requirements.txt
 echo "Run harvest setup in the background"
